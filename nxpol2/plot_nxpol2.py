b20b5c99f7c0d5197461139fc8c94ec0d5a9d7ef
Joshua Hampton <joshua.hampton@live.co.uk>
2023-05-22 13:32:59 +0000

    Use sweep time in image filename

--- nxpol2/plot_nxpol2.py
+++ nxpol2/plot_nxpol2.py
@@ -11,6 +11,8 @@ import json
 import sys
 from utils import wescon_kml_grid
 import os
+import datetime as dt
+
 
 variables = ["dBZ", "ZDR", "KDP", "RhoHV", "V"]
 desired_elevations = [1.0, 2.0, 3.0]
@@ -131,8 +133,9 @@ def make_ppi_plots(
 
             plt.xlim(xlim)
             plt.ylim(ylim)
+            sweep_time = dt.datetime.strftime(pyart.graph.common.generate_radar_time_sweep(radar,sweep),"%Y%m%d%H%M%S")
             plt.savefig(
-                f"{outdir}/{sweep_elevation}deg/{radar.metadata['start_datetime'].replace('-','').replace(':','').replace('Z','').replace('T','')}00{var}.ppi.png"
+                f"{outdir}/{sweep_elevation}deg/{sweep_time}00{var}.ppi.png"
             )
             plt.close()
 
@@ -161,8 +164,9 @@ def make_rhi_plots(radar, radar_name, outdir, var, var_scales, ylim=[0, 15]):
     )
     plt.grid(linewidth=1, color="gray", alpha=0.3, linestyle="--")
     plt.ylim(ylim)
+    sweep_time = dt.datetime.strftime(pyart.graph.common.generate_radar_time_sweep(radar,0),"%Y%m%d%H%M%S")
     plt.savefig(
-        f"{outdir}/{radar_name}_{radar.metadata['start_datetime'].replace('-','').replace(':','')}_{var}_rhi.png"
+        f"{outdir}/{radar_name}_{sweep_time}00{var}.rhi.png"
     )
     plt.close()
 
